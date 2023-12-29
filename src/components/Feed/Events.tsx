e88c288d3cebfbf21c0f21266e68e5a9538e2c45
pokemau <rentillosa90@gmail.com>
2023-12-29 17:32:27 +0800

    create ui for individual event

--- src/components/Feed/Events.tsx
+++ src/components/Feed/Events.tsx
@@ -11,6 +11,9 @@ import {
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
+// ICONS
+import { BiUpvote } from "react-icons/bi";
+
 const Events = () => {
   const [eventsList, setEventsList] = useState<DocumentData[]>([]);
   const [limitCount, setLimitCount] = useState(30);
@@ -32,9 +35,6 @@ const Events = () => {
 
   return (
     <div>
-      <button onClick={() => console.log(eventsList[0].data())}>
-        CLICK HERE
-      </button>
       {eventsList.length != 0 ? (
         <Event eventsList={eventsList} />
       ) : (
@@ -62,12 +62,27 @@ const Event: React.FC<EventProps> = ({ eventsList }) => {
   return (
     <>
       {eventsList?.map((ev, index) => (
-        <div key={ev.id} className="bg-red-400">
-          <h1>{ev.data().EventTitle}</h1>
-          <p>{ev.data().EventDescription}</p>
-          <p>{displayDate(ev.data().EventDate?.toDate())}</p>
-          <p>{`Participants: ${ev.data().EventParticipantCount}`}</p>
-          <p>{`Upvotes: ${ev.data().EventUpvoteCount}`}</p>
+        <div
+          key={ev.id}
+          className="bg-[#f5f5f5] rounded-lg w-[50%] my-2 mx-auto p-4">
+          <div className="flex items-center gap-4 border-gray-300 border-b-[1px] py-1">
+            <h1 className="font-bold text-3xl">{ev.data().EventTitle}</h1>
+            <p className="text-[#6d6d6d]">
+              on {displayDate(ev.data().EventDate?.toDate())}
+            </p>
+          </div>
+
+          <div className="my-2">
+            <p className="break-words text-xl">{ev.data().EventDescription}</p>
+            <p>{`Participants: ${ev.data().EventParticipantCount}`}</p>
+          </div>
+
+          <div className="flex items-center">
+            <div className="text-[1.5rem] cursor-pointer hover:text-red-500">
+              <BiUpvote />
+            </div>
+            <p className="text-[1.1rem]">{ev.data().EventUpvoteCount}</p>
+          </div>
         </div>
       ))}
     </>
