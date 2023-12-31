ebeb730142b89a1551039a2d748c0a26f21fe2b2
pokemau <rentillosa90@gmail.com>
2023-12-31 18:11:18 +0800

    move generate new user data func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,19 @@
 "use client";
 
-import { auth, db } from "@/auth/firebase";
+import { auth } from "@/auth/firebase";
 import Feed from "@/components/Feed/Feed";
 import PageLoad from "@/components/Loading/PageLoad";
 import Login from "@/components/Menu/Login";
-import { UserDataType } from "@/utils/Intefaces";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import GenerateNewUserData from "@/utils/User/GenerateNewUserData";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
 
-  const setUserDataIfNotExists = async (userId: string) => {
-    try {
-      const userDocRef = doc(db, "users_list", userId);
-      const userDocSnap = await getDoc(userDocRef);
-
-      if (!userDocSnap.exists()) {
-        const docRef = doc(db, "users_list", userId);
-        if (user) {
-          const d: UserDataType = {
-            uid: user.uid,
-            userType: "user",
-            upVotes: [],
-            eventsJoined: [],
-          };
-          await setDoc(docRef, d);
-        }
-      }
-    } catch {
-      console.log("ERRRRRRRRRRRRRRRRRR");
-    }
-  };
-
   if (loading) return <PageLoad />;
 
   if (user) {
-    setUserDataIfNotExists(user.uid);
+    GenerateNewUserData(user);
     return (
       <div>
         <Feed user={user} />
---
+++ src/utils/User/GenerateNewUserData.tsx
@@ -0,0 +1,24 @@
+import { db } from "@/auth/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { UserDataType } from "../Intefaces";
+import { User } from "firebase/auth";
+
+const GenerateNewUserData = async (user: User) => {
+  const userDocRef = doc(db, "users_list", user.uid);
+  const userDocSnap = await getDoc(userDocRef);
+
+  if (!userDocSnap.exists()) {
+    const docRef = doc(db, "users_list", user.uid);
+    if (user) {
+      const d: UserDataType = {
+        uid: user.uid,
+        userType: "user",
+        upVotes: [],
+        eventsJoined: [],
+      };
+      await setDoc(docRef, d);
+    }
+  }
+};
+
+export default GenerateNewUserData;
