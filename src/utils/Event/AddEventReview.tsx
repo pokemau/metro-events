c1caa4ecdbc0307c51e01f39c56a6b38591ab2bb
pokemau <rentillosa90@gmail.com>
2023-12-31 18:00:10 +0800

    fix add event review func

--- src/utils/Event/AddEventReview.tsx
+++ src/utils/Event/AddEventReview.tsx
@@ -1,19 +1,15 @@
 import { db } from "@/auth/firebase";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, updateDoc } from "firebase/firestore";
 import { EventParamsType, ReviewParamsType } from "../Intefaces";
 
 const AddEventReview = async (eventID: string, newReview: ReviewParamsType) => {
   try {
-    const documentRef = doc(db, "events", eventID);
-    const docSnap = await getDoc(documentRef);
-    const eventData = docSnap.data() as EventParamsType;
+    const eventRef = doc(db, "events", eventID);
 
-    const updatedEventData = {
-      ...eventData,
-      EventReviews: [...eventData.EventReviews, newReview],
-    };
+    await updateDoc(eventRef, {
+      EventReviews: arrayUnion(newReview),
+    });
 
-    await updateDoc(documentRef, updatedEventData);
     console.log("ADDED REVIEW");
   } catch {
     console.log("FAILED TO ADD REVIEW");
