f48d3e6bf8f0daa18c4e320e714dedb54a6af663
pokemau <rentillosa90@gmail.com>
2023-12-31 18:00:32 +0800

    remove unused

--- src/utils/Event/AddEventReview.tsx
+++ src/utils/Event/AddEventReview.tsx
@@ -1,6 +1,6 @@
 import { db } from "@/auth/firebase";
-import { arrayUnion, doc, getDoc, updateDoc } from "firebase/firestore";
-import { EventParamsType, ReviewParamsType } from "../Intefaces";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { ReviewParamsType } from "../Intefaces";
 
 const AddEventReview = async (eventID: string, newReview: ReviewParamsType) => {
   try {
